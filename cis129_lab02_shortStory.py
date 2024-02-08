42eeac23fead4b8d7965344ee3d0514abea88b9f
Richard6592 <159187390+Richard6592@users.noreply.github.com>
2024-02-08 13:28:35 -0700

    Update cis129_lab02_shortStory.py
    I added all print statements for assigned Module 2 lab

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+#Author: Richard Valenzuela
+#My first Python lab in my CIS129 course
+# Prints a simple short story 
+print('hi')
+print('how are you')
+print('good, how are you')
+print('I am also good')
+print('bye')
